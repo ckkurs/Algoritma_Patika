a420d990ac90847a4319dd01d9c33ef10abc9202
cemkndz <120122784+ckkurs@users.noreply.github.com>
2023-02-24 19:49:27 +0300

    Alogirtma_Patika

---
+++ Algoritma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime ve çıkarmak istediğin karakter indeksini gir (Arada virgül olmalıdır):");
+            string a = Console.ReadLine();
+
+            string[] input = a.Split(",");
+            string word = input[0];
+            int number = Convert.ToInt32(input[1]);
+            string newWord = word.Remove(number, 1);
+
+            Console.WriteLine(newWord);
+        }
+    }
+}
